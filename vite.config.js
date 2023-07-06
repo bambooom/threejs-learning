f08792582fb9f7740bb4b7b02b1f3a22c616ac43
bamboo <nmz89825@gmail.com>
2023-07-06 13:11:15 +0800

    :art: Refactor: rename html files.

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: resolve(__dirname, 'index.html'),
-        fundamentals: resolve(__dirname, 'examples/01-fundamental.html'),
-        primitives: resolve(__dirname, 'examples/02-primitives.html'),
-        scenegraph: resolve(__dirname, 'examples/03-scenegraph.html'),
+        fundamentals: resolve(__dirname, 'examples/fundamental.html'),
+        primitives: resolve(__dirname, 'examples/primitives.html'),
+        scenegraph: resolve(__dirname, 'examples/scenegraph.html'),
         tank: resolve(__dirname, 'examples/tank.html'),
-        texture: resolve(__dirname, 'examples/04-texture.html'),
+        texture: resolve(__dirname, 'examples/texture.html'),
         lights: resolve(__dirname, 'examples/lights.html'),
         cameras: resolve(__dirname, 'examples/cameras.html'),
       },
